ba3f49477ab9478690a52f7fb94427967dbb7f43
slavasit <slavasit@gmail.com>
2017-05-10 13:05:18 +0300

    Design Table

--- index.php
+++ index.php
@@ -1,7 +1,48 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: slavas
- * Date: 5/10/2017
- * Time: 12:39 PM
- */
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <title>Document</title>
+    <script
+        src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+        crossorigin="anonymous">
+
+    </script>
+
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1 style="text-align: center; text-transform: uppercase">To Do List</h1>
+            <div class="col-md-10 col-md-offset-1">
+                <table class="table">
+                    <button type="button" class="btn btn-success">Add Task</button>
+                    <button type="button" style="float: right" class="btn btn-default">Print</button>
+                    <hr>
+                    <br>
+                    <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Task</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
